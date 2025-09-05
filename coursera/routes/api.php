854643a32c1e7ad131cd82ade224679fe4922db2
Andrea Vrljes <andrea.vrljes@gmail.com>
2025-09-05 13:32:29 +0200

    Controllers and routs

--- coursera/routes/api.php
+++ coursera/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\EnrollmentController;
+use App\Http\Controllers\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/search', [CourseController::class, 'searchCourses']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
+
+Route::get('/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
+
+Route::get('/enrollments', [EnrollmentController::class, 'index']);
+Route::get('/enrollments/{id}', [EnrollmentController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('courses', CourseController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('lessons', LessonController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('enrollments', EnrollmentController::class)
+        ->only(['store', 'destroy']);
+
+    Route::put('/users/update-role', [AuthController::class, 'updateRole']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
