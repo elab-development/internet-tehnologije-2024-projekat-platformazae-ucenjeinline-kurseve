2c04b15fcbc96eb2a99849ea54d91f1a3ac01155
Andrea Vrljes <andrea.vrljes@gmail.com>
2025-09-16 13:30:02 +0200

    Laravel - Course: kontroler i rute

--- coursera/routes/api.php
+++ coursera/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +20,17 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/search', [CourseController::class, 'searchCourses']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+     Route::resource('courses', CourseController::class)
+        ->only(['store', 'update', 'destroy']);
+        
     Route::put('/users/update-role', [AuthController::class, 'updateRole']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
